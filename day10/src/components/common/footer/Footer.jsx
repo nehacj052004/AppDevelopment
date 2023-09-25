646a9e27969e4ea5cb7db62986f64e00cdc425fd
nehacj052004 <119111551+nehacj052004@users.noreply.github.com>
2023-09-25 15:58:55 +0530

    Add files via upload

---
+++ day10/src/components/common/footer/Footer.jsx
@@ -0,0 +1,97 @@
+import React from "react"
+import { blog } from "../../../dummydata"
+import "./footer.css"
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <>
+      <section className='newletter'>
+        <div className='container flexSB'>
+          <div className='left row'>
+            <h1>Newsletter - Stay tune and get the latest update</h1>
+          </div>
+          <div className='right row'>
+            <input type='text' placeholder='Enter email address' />
+            <i className='fa fa-paper-plane'></i>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className='container padding'>
+          <div className='box logo'>
+            <h1>ADMISSION BOX</h1>
+            <span></span>
+            <p>A place where your kid have a bright future.</p>
+
+            <i className='fab fa-facebook-f icon'></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-instagram icon'></i>
+          </div>
+          <div className='box link'>
+            <h3>Explore</h3>
+            <ul>
+              <li>About Us</li>
+              <li>Services</li>
+              <li>Courses</li>
+              <li>Blog</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+          <div className='box link'>
+            <h3>Quick Links</h3>
+            <ul>
+              <li>Contact Us</li>
+              <li>Feedbacks</li>
+            </ul>
+          </div>
+          <div className='box'>
+            <h3></h3>
+            {blog.slice(0, 3).map((val) => (
+              <div className='items flexSB'>
+                <div className='img'>
+                  <img src={val.cover} alt='' />
+                </div>
+                <div className='text'>
+                  <span>
+                    <i className='fa fa-calendar-alt'></i>
+                    <label htmlFor=''>{val.date}</label>
+                  </span>
+                  <span>
+                    <i className='fa fa-user'></i>
+                    <label htmlFor=''>{val.type}</label>
+                  </span>
+                  <h4>{val.title.slice(0, 40)}...</h4>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='box last'>
+            <h3>Have a Questions?</h3>
+            <ul>
+              <li>
+                <i className='fa fa-map'></i>
+                Email us right now
+              </li>
+              <li>
+                <i className='fa fa-paper-plane'></i>
+                info@yourdomain.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      <div className='legal'>
+<Link to='/privacy'><i className='fa fa-heart'>Privacy Policy</i></Link>
+        <p>
+        </p>
+<br></br>
+<Link to='/terms'>
+<i class Name='fa-fa-heart'>Terms and condition</i>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Footer
