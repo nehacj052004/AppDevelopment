2b389f2c59aadf34db56839cf9a2e65772694aec
nehacj052004 <119111551+nehacj052004@users.noreply.github.com>
2023-10-05 15:46:41 +0530

    Add files via upload

---
+++ day15/neha/school/admission/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package mridini.school.admission.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import mridini.school.admission.constant.Api;
+import mridini.school.admission.dto.request.AuthenticationRequest;
+import mridini.school.admission.dto.request.RegisterRequest;
+import mridini.school.admission.dto.response.AuthenticationResponse;
+import mridini.school.admission.service.AuthService;
+
+@RestController
+@RequestMapping(Api.AUTH)
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        boolean isRegistered = authService.userRegistration(request);
+        return isRegistered ? ResponseEntity.ok("User registered successfully")
+                : ResponseEntity.badRequest().body("Sommething went wrong!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authService.userAuthentication(request));
+    }
+}
