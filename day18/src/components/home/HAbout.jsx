c6d7b22db73cec9cfdc08913fef62d1a792f0eba
nehacj052004 <119111551+nehacj052004@users.noreply.github.com>
2023-10-11 15:16:54 +0530

    Add files via upload

---
+++ day18/src/components/home/HAbout.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import OnlineCourses from "../allcourses/OnlineCourses"
+import Heading from "../common/heading/Heading"
+import "../allcourses/courses.css"
+import { coursesCard } from "../../dummydata"
+
+const HAbout = () => {
+  return (
+    <>
+      <section className='homeAbout'>
+        <div className='container'>
+          <Heading subtitle='our teachers' title='Role Models' />
+
+          <div className='coursesCard'>
+            {/* copy code form  coursesCard */}
+            <div className='grid2'>
+              {coursesCard.slice(0, 3).map((val) => (
+                <div className=''>
+                 
+                      <div className='coursescardimagediv'>
+                        <img src={val.cover} alt='' className="coursescardimage" />
+                      </div>
+                    
+        
+                  
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <OnlineCourses />
+      </section>
+    </>
+  )
+}
+
+export default HAbout
---
+++ day18/src/main/java/trailblazers/school12/School12Application.java
@@ -0,0 +1,14 @@
+package trailblazers.school12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School12Application.class, args);
+		System.out.println("Done😎");
+	}
+
+}
