3bd14148369e4031c95c1e0b7e64aa78d304e9a9
nehacj052004 <119111551+nehacj052004@users.noreply.github.com>
2023-10-16 12:15:18 +0530

    Delete day21/frontent directory

--- day21/frontent/services/axiosEndpoints.jsx
+++
@@ -1,8 +0,0 @@
-const API_ENDPOINTS={
-    LOGIN: '/api/v1/auth/login',
-    SIGNUP: '/api/v1/auth/register',
-    PROFILE: '/api/v1/user',
-};
-
-
-export default API_ENDPOINTS;
--- day21/frontent/services/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './userSlice'
-import adminReducer from './adminSlice'
-export default configureStore({
-    reducer :{
-        user:userReducer,
-        admin: adminReducer,
-    }
-})
