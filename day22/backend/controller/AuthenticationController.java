c187e241e7212db757ecd82b3d17805e9a358f2f
nehacj052004 <119111551+nehacj052004@users.noreply.github.com>
2023-10-16 12:04:28 +0530

    Add files via upload

---
+++ day22/backend/controller/AuthenticationController.java
@@ -0,0 +1,36 @@
+package trailblazers.school12.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import trailblazers.school12.constant.Api;
+import trailblazers.school12.dto.request.AuthenticationRequest;
+import trailblazers.school12.dto.request.RegisterRequest;
+import trailblazers.school12.dto.response.AuthenticationResponse;
+import trailblazers.school12.service.AuthService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(Api.AUTH)
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        boolean isRegistered = authService.userRegistration(request);
+        return isRegistered ? ResponseEntity.ok("User registered successfully")
+                : ResponseEntity.badRequest().body("Sommething went wrong!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authService.userAuthentication(request));
+    }
+}
+
